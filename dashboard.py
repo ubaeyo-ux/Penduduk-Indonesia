0f4b015feceebcb3eb31657faf49b1a64aa72428
ubaeyo <181945524+ubaeyo-ux@users.noreply.github.com>
2024-09-20 15:07:16 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -95,9 +95,9 @@ def make_choropleth(input_df, input_id, input_column, input_color_theme):
             locations=df["Column"],  # Spatial coordinates
             featureidkey="properties.Propinsi",
             z=df["Columnnext"],  # Data to be color-coded
-            color_continuous_scale=input_color_theme,
-            range_color=(0, max(df_selected_year.population)),
-            labels={'population':'Population'}
+            colorbar=input_color_theme,
+            colorscale=(0, max(df_selected_year.population)),
+            labels={'population':'Jumlah'}
         )
     )
     choropleth_map.update_geos(fitbounds="locations", visible=False)
